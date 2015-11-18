1d3a02b01354195e6dce23beadc8df1fe712aa0f
b1uec0in <b1uec0in@naver.com>
2015-11-18 13:22:21 +0900

    initial commit

---
+++ common.js
@@ -0,0 +1,144 @@
+var printLogTime = true;
+
+function log(text) {
+    addLog();
+    var date = new Date();
+    var timeStamp = "";
+    if (printLogTime) {
+        timeStamp = ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + ":" + ("0" + date.getSeconds()).slice(-2) + "." + ("00" + date.getMilliseconds()).slice(-3);
+    }
+    document.getElementById("status").innerHTML = document.getElementById("status").innerHTML + timeStamp + "&nbsp;&nbsp;&nbsp; " + text + "<br/>";
+}
+
+var headerAdded = false;
+function addHeader() {
+    if (headerAdded)
+        return;
+    headerAdded = true;
+
+    var bodyText = document.body.innerHTML;
+    var header = '<h3> <a href="javascript:history.go(-1)" style="text-decoration:none;font-size:1.5em">&#x2039;&nbsp;&nbsp;</a> &nbsp;&nbsp;' + document.title + '</h3><hr />';
+
+    document.body.innerHTML = header + bodyText;
+}
+
+var logAdded = false;
+function addLog() {
+    if (logAdded)
+        return;
+    logAdded = true;
+    var div = document.createElement("div");
+    document.body.appendChild(div);
+    div.innerHTML = '<div>Log :</div><div style="background:black;margin-top:0;"><span id="status" style="font-size:11px;10px;color:#80f080;background:black;"></div></p>';
+
+}
+
+function processPassFailButton() {
+    var tags = document.getElementsByClassName("passFail");
+
+    for (var i = 0; i < tags.length; ++i) {
+        var element = tags[i];
+        var id = element.id;
+
+        element.addEventListener("click", function (e) {
+            changePassFailStatus(e.target);
+        });
+        updatePassFailStatus(id);
+    }
+}
+
+function changePassFailStatus(element) {
+    var passFail = eval("localStorage." + element.id);
+    if (passFail == "PASS") {
+        passFail = "FAIL";
+    } else if (passFail == "FAIL") {
+        passFail = "NA";
+    } else if (passFail == "NA") {
+        passFail = "NT";
+    } else {
+        passFail = "PASS";
+    }
+    eval("localStorage." + element.id + "= '" + passFail + "'");
+    updatePassFailStatus(element.id)
+}
+
+function isAndroid() {
+    if (navigator.userAgent.indexOf("Android") >= 0)
+        return true;
+    return false;
+}
+
+var isAndroid = navigator.userAgent.indexOf("Android") >= 0;
+var isiOS = navigator.userAgent.indexOf("iPhone") >= 0 || navigator.userAgent.indexOf("iPad") >= 0 || navigator.userAgent.indexOf("iPod") >= 0;
+
+function getAndroidVersion() {
+    var ua = navigator.userAgent.toLowerCase();
+    var match = ua.match(/android\s([0-9\.]*)/);
+    return match ? match[1] : 0;
+};
+
+var androidVersion = parseFloat(getAndroidVersion());
+
+function getDefault(id) {
+    if (id == "A04" || id == "A06") {
+        if (!isAndroid)
+            return "NA";
+    } else if (id == "A07") {
+        if (!isiOS)
+            return "NA";
+    } else if (id == "H01") {
+        if (isiOS)
+            return "NA";
+        if (isAndroid && androidVersion < 5.0)
+            return "NA";
+    } else if (id == "I01") {
+        if (isiOS)
+            return "NA";
+        if (isAndroid && androidVersion < 4.4)
+            return "NA";
+    }
+
+}
+
+function updatePassFailStatus(id) {
+    var passFail = "NT";
+    var element = document.getElementById(id);
+    if (element != null) {
+        passFail = eval("localStorage." + id);
+        if (passFail == null) {
+            passFail = getDefault(id);
+            if (passFail != null) {
+                eval("localStorage." + id + "= '" + passFail + "'");
+            }
+        }
+        if (passFail == "PASS") {
+            element.innerHTML = "PASS";
+            element.style.background = "#a0ff80";
+        } else if (passFail == "FAIL") {
+            element.innerHTML = "FAIL";
+            element.style.background = "#ffa0a0";
+        } else if (passFail == "NA") {
+            element.innerHTML = "N/A";
+            element.style.background = "#a0a0a0";
+        } else {
+            element.innerHTML = "N/T";
+            element.style.background = "#ffffa0";
+        }
+    }
+}
+
+function onDOMContentLoaded() {
+    addHeader();
+
+    if (typeof (test) == "function") {
+        window.setTimeout(function () {
+            test();
+        }, 700);
+    }
+
+    processPassFailButton();
+}
+
+//window.addEventListener('load', wrapBody, false)
+document.addEventListener('DOMContentLoaded', onDOMContentLoaded, false);
+
